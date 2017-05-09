a9d0bafbdae73f8a9d9a6143122e1c4d5cd32ad3
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-05-09 14:31:17 -0400

    refactor nav font-size and add package.json and yarn.lock to branch

--- src/modules/nav/nav.jsx
+++ src/modules/nav/nav.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 function Nav() {
+    const title = {
+        fontSize: 65
+    };
+
     return(
         <div className={'row'}>
-            <h1 className={'six columns offset-by-three'}>Squid Ink</h1>
+            <h1 className={`six columns offset-by-three`} style={title}>Squid Ink</h1>
         </div>
     )
 }
