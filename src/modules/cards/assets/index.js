1df51b1730cc51b4fd2deed75fc98493ce4a5f40
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-06-09 17:21:41 -0400

    add new gifs and update index.js

--- src/modules/cards/assets/index.js
+++ src/modules/cards/assets/index.js
@@ -2,18 +2,20 @@ import bonerJimi from './gifs/bonerJimi.gif';
 import demboysSM from './gifs/demboysSM.gif';
 import dudu2 from './gifs/dudu2.gif';
 import dumbcop2 from   './gifs/dumbcop2.gif';
-// import fruits from  './gifs/fruits.gif';
+import fruits from  './gifs/fruitssmall.gif';
 import richandianknife from  './gifs/richandianknife.gif';
 import trumpPick from  './gifs/trumpPick.gif';
-// import yaHey from  './gifs/yaHey.gif';
+import yaHey from  './gifs/yaHeysmall.gif';
 
 const gifList = [
-    bonerJimi,
     demboysSM,
+    trumpPick,
+    yaHey,
     dudu2,
     dumbcop2,
     richandianknife,
-    trumpPick
+    bonerJimi,
+    fruits
 ];
 
 export default (gifList)
