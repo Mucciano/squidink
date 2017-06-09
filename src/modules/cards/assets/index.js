ea7bce36eedd6f6c957ed1c853d22b2379e98840
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-06-09 12:37:32 -0400

    remove oversize gif files

--- src/modules/cards/assets/index.js
+++ src/modules/cards/assets/index.js
@@ -2,20 +2,18 @@ import bonerJimi from './gifs/bonerJimi.gif';
 import demboysSM from './gifs/demboysSM.gif';
 import dudu2 from './gifs/dudu2.gif';
 import dumbcop2 from   './gifs/dumbcop2.gif';
-import fruits from  './gifs/fruits.gif';
+// import fruits from  './gifs/fruits.gif';
 import richandianknife from  './gifs/richandianknife.gif';
 import trumpPick from  './gifs/trumpPick.gif';
-import yaHey from  './gifs/yaHey.gif';
+// import yaHey from  './gifs/yaHey.gif';
 
 const gifList = [
     bonerJimi,
     demboysSM,
     dudu2,
     dumbcop2,
-    fruits,
     richandianknife,
-    trumpPick,
-    yaHey
+    trumpPick
 ];
 
 export default (gifList)
