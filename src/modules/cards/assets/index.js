e921be4ad3e9621776f5b674af5714a0c3daf66b
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-06-22 12:05:25 -0400

    add new gif

--- src/modules/cards/assets/index.js
+++ src/modules/cards/assets/index.js
@@ -5,12 +5,14 @@ import dumbcop2 from   './gifs/dumbcop2.gif';
 import fruits from  './gifs/fruitssmall.gif';
 import richandianknife from  './gifs/richandianknife.gif';
 import trumpPick from  './gifs/trumpPick.gif';
-import yaHey from  './gifs/yaHeysmall.gif';
+import yaHeysmall from  './gifs/yaHeysmall.gif';
+import pubicWebSmall from './gifs/pubicWebSmall.gif';
 
 const gifList = [
+    pubicWebSmall,
     demboysSM,
     trumpPick,
-    yaHey,
+    yaHeysmall,
     dudu2,
     dumbcop2,
     richandianknife,
